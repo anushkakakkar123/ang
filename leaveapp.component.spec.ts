f8e442c5f39a569f0e3ddb15a00bd695bec6d216
anushkakakkar123 <127036271+anushkakakkar123@users.noreply.github.com>
2023-08-16 12:50:14 +0530

    Add files via upload

--- leaveapp.component.spec.ts
+++ leaveapp.component.spec.ts
@@ -1,57 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { LeaveAppComponent } from './leaveapp.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('LeaveAppComponent', () => {
-  let component: LeaveAppComponent;
-  let fixture: ComponentFixture<LeaveAppComponent>;
+let component: LeaveAppComponent;
+let fixture: ComponentFixture<LeaveAppComponent>;
 
-  beforeEach(async () => {
-    TestBed.configureTestingModule({
-      declarations: [LeaveAppComponent],
-    });
-    fixture = await TestBed.createComponent(LeaveAppComponent);
-    component = fixture.componentInstance;
-  });
+beforeEach(async(() => {
+TestBed.configureTestingModule({
+declarations: [LeaveAppComponent],
+imports: [ReactiveFormsModule, HttpClientTestingModule],
+providers: [{ provide: ComponentFixtureAutoDetect, useValue: true }]
+}).compileComponents();
+}));
 
-  it('should create the component', () => {
-    expect(component).toBeTruthy();
-  });
+beforeEach(() => {
+fixture = TestBed.createComponent(LeaveAppComponent);
+component = fixture.componentInstance;
+});
 
-  it('should have a form with the correct properties', async () => {
-    await fixture.whenStable();
-    const form = component.addLeave;
-    expect(form.controls['name'].value).toBe('');
-    expect(form.controls['leave_type'].value).toBe('');
-    expect(form.controls['start_date'].value).toBe('');
-    expect(form.controls['end_date'].value).toBe('');
-    expect(form.controls['teams'].value).toBe('');
-    expect(form.controls['file'].value).toBe('');
-    expect(form.controls['reporter'].value).toBe('');
-  });
+it('should create the component', () => {
+expect(component).toBeTruthy();
+});
 
-  it('should disable the submit button if the form is invalid', async () => {
-    await fixture.whenStable();
-    const form = component.addLeave;
-    form.controls['name'].setValue('');
-    form.controls['leave_type'].setValue('');
-    form.controls['start_date'].setValue('');
-    form.controls['end_date'].setValue('');
-    form.controls['teams'].setValue('');
-    form.controls['file'].setValue('');
-    form.controls['reporter'].setValue('');
-    const debugElement = fixture.debugElement.query(By.css('button[type="submit"]'));
-    expect(debugElement.querySelector('button[type="submit"]').disabled).toBe(true);
-  });
+it('should be invalid when the form is empty', () => {
+expect(component.addLeave.valid).toBeFalsy();
+});
 
-  it('should enable the submit button if the form is valid', async () => {
-    await fixture.whenStable();
-    form.controls['name'].setValue('John Doe');
-    form.controls['leave_type'].setValue('Casual Leave');
-    form.controls['start_date'].setValue('2023-01-01');
-    form.controls['end_date'].setValue('2023-01-03');
-    form.controls['teams'].setValue('Engineering');
-    form.controls['file'].setValue('');
-    form.controls['reporter'].setValue('Shivay');
-    expect(fixture.element.querySelector('button[type="submit"]').disabled).toBe(false);
-  });
+
+it('should disable the submit button if the form is invalid', () => {
+const form = component.addLeave;
+// Do not set any values, the form will remain invalid
+fixture.detectChanges();
+const submitButton: HTMLButtonElement = fixture.nativeElement.querySelector('button[type="submit"]');
+expect(submitButton.disabled).toBe(true);
+});
 });
